20783934e8ea886c4adf07a1d26505c3818ed266
RYOMA1205 <ryoma.take@gmail.com>
2021-07-25 00:09:13 +0900

    コミット　動画13まで完了

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -15,13 +15,26 @@ public class ZombieController : MonoBehaviour
     public enum STATE {IDLE, WANDER, ATTACK, CHASE, DEAD};
     public STATE state = STATE.IDLE;
 
-    // スタート時に変数にコンポーネントを格納
+    // 変数の宣言(プレイヤーオブジェクト格納:走るスピード)
+    GameObject target;
+    public float runSpeed;
 
-    // Start is called before the first frame update
+    // プレイヤーとの距離関数を作成
+    // 発見判定関数作成
+    // 見失ったか判定する関数
+
+    // 列挙型にコードを追加記述
+
+    // スタート時に変数にコンポーネントを格納
     void Start()
     {
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
     }
 
     public void TurnOffTrigger()
@@ -30,6 +43,31 @@ public class ZombieController : MonoBehaviour
         animator.SetBool("Run", false);
         animator.SetBool("Death", false);
         animator.SetBool("Attack", false);
+    }
+
+    float DistanceToPlayer()
+    {
+        return Vector3.Distance(target.transform.position, transform.position);
+    }
+
+    bool CanSeePlayer()
+    {
+        if (DistanceToPlayer() < 15)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool ForGetPlayer()
+    {
+        if (DistanceToPlayer() > 20)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     // Update is called once per frame
@@ -42,10 +80,15 @@ public class ZombieController : MonoBehaviour
             case STATE.IDLE:
                 TurnOffTrigger();
 
-                if(Random.Range(0, 100) < 5)
+                if (CanSeePlayer())
+                {
+                    state = STATE.CHASE;
+                }
+                else if(Random.Range(0, 100) < 5)
                 {
                     state = STATE.WANDER;
                 }
+
                 break;
 
             case STATE.WANDER:
@@ -69,6 +112,28 @@ public class ZombieController : MonoBehaviour
                 {
                     state = STATE.IDLE;
                     agent.ResetPath();
+                }
+
+                if (CanSeePlayer())
+                {
+                    state = STATE.CHASE;
+                }
+
+                break;
+
+            case STATE.CHASE:
+                agent.SetDestination(target.transform.position);
+                agent.stoppingDistance = 3;
+
+                TurnOffTrigger();
+
+                agent.speed = runSpeed;
+                animator.SetBool("Run", true);
+
+                if (ForGetPlayer())
+                {
+                    agent.ResetPath();
+                    state = STATE.WANDER;
                 }
 
                 break;
